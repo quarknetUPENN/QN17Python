8f5d337061efa6d7ea47fea4cac7e99d62d949c4
Jonathan Okasinksi <nitrocalcite@users.noreply.github.com>
2017-06-30 08:10:41 -0400

    Change plot config vars to use a dict, minor clean up

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# The main script.  Run this script to analyze a .gon event file
+# The main script.  Run this script to analyze a folder of .gon event files
 from holder import *
 import circlecalc as cc
-import numpy as np
 import matplotlib.pyplot as plt
+from numpy import loadtxt
 from glob import glob
 import os
 
@@ -12,23 +12,23 @@ dataDir = "sampledata/"
 # in dataDir, it will be OVERWRITTEN
 imgDir = "images"
 # define what to plot
-plotTubes = True
-plotTubeLabels = True
-plotPaddles = True
-plotHitCircles = True
-plotAllPossibleTanLines = True
-plotPaddleTanLines = True
-plotAverageTanLine = True
+plotConfig = {"showTubes": True,
+              "showTubeLabels": True,
+              "showPaddles": True,
+              "showHitCircles": True,
+              "showAllPossibleTanLines": True,
+              "showPaddleTanLines": True,
+              "showAverageTanLine": True}
 
 # ******************Load tube position data***************** #
-# get the x,y positions of each tube in m.  load them into a dict using names as keys
+# using numpy, get the x,y positions of each tube in m.  load them into a dict using names as keys
 # requires name to be converted from bytes to strings
 tubepos = {}
-for tube in np.loadtxt("tubepos.csv", delimiter=",", dtype="S3,f4,f4"):
+for tube in loadtxt("tubepos.csv", delimiter=",", dtype="S3,f4,f4"):
     tubepos[tube[0].decode("utf-8")] = (tube[1], tube[2])
 
 # Move to the directory in which the data files are
-os.chdir(dataDir)  # from os
+os.chdir(dataDir)
 # Make a directory to fill with the produced images.  if it already exists, overwrite!
 os.makedirs(imgDir, exist_ok=True)
 # Iterate through every data file in the directory, generating an output image for them
@@ -43,7 +43,7 @@ for gon in glob("*.gon"):
             data = line[0:-1].split(";")
 
             # if it's code 256, meaning the tube didn't fire, ignore it, we don't care
-            # otherwise, convert # of clock pulses into radius (m) and xref with the tube x,y pos to make a tubehit event
+            # otherwise convert # of clock pulses into radius (m) and xref with the tube x,y pos to make a tubehit event
             if data[1] != "256":
                 xy = tubepos[data[0]]
                 dataArray.append(tubehit(xy[0], xy[1], float(data[1]) * 1e-8 * DRIFT_VELOCITY, data[0]))
@@ -69,37 +69,37 @@ for gon in glob("*.gon"):
     # create the drawing
     fig, ax = plt.subplots()
     # draw all tubes if requested
-    if plotTubes:
+    if plotConfig["showTubes"]:
         for name, pos in tubepos.items():
             ax.add_artist(plt.Circle(pos, OUTER_RADIUS, fill=False, color='black'))
-            if plotTubeLabels:
+            if plotConfig["showTubeLabels"]:
                 ax.text(pos[0], pos[1], name, size=8, ha="center", va="center")
     # draw all hit circles if requested
-    if plotHitCircles:
+    if plotConfig["showHitCircles"]:
         for hit in dataArray:
             ax.add_artist(plt.Circle((hit.x, hit.y), hit.r, fill=False, color='blue'))
     # draw both paddles if requested
-    if plotPaddles:
+    if plotConfig["showPaddles"]:
         ax.plot([PADDLE_MIN_X, PADDLE_MAX_X], [PADDLE_MIN_Y, PADDLE_MIN_Y], color='black', lw=10, label="Scintillator Paddle")
         ax.plot([PADDLE_MIN_X, PADDLE_MAX_X], [PADDLE_MAX_Y, PADDLE_MAX_Y], color='black', lw=10)
     # draw all possible tan lines if requested
-    if plotAllPossibleTanLines:
+    if plotConfig["showAllPossibleTanLines"]:
         for line in rawTanList:
             lab, = ax.plot([0, 1], [line.y(0), line.y(1)], color="green", ls="--", lw=0.5)
         lab.set_label("All Tangent Lines")
     # draw all tan lines through paddle if requested
-    if plotPaddleTanLines:
+    if plotConfig["showPaddleTanLines"]:
         for line in paddleTanList:
             lab, = ax.plot([0, 1], [line.y(0), line.y(1)], color="green", ls="-", lw=1)
         lab.set_label("Paddle Tangent Lines")
     # draw average tan line if requested
-    if plotAverageTanLine:
+    if plotConfig["showAverageTanLine"]:
         lab, = ax.plot([0, 1], [avgLine.y(0), avgLine.y(1)], color="red", ls="-", lw=2)
         lab.set_label("Average Tangent Line")
     # draw legend to right of graph
     lgd = ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-    # size the drawing and save it
+    # size the drawing and save it into the imgDir
     plt.xlim((0, 0.5))
     plt.ylim((0, 0.5))
     fig.savefig(imgDir+"/"+gon[:-4]+".png", bbox_extra_artists=(lgd,), bbox_inches="tight")
